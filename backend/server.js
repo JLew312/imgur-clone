7fbe6207b011135a370ecf897808af8fe378d8eb
Josh <jrlewis312gmail.com>
2023-02-23 23:02:03 -0800

    express server built

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+const port = 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.send("Hello from the inside!");
+})
+
+app.listen(port, () => console.log(`Server has started on port ${port}!`));
